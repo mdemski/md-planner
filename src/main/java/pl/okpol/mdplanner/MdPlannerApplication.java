c4c761992a08edeb83e90032131eec58d341081d
mardem <m.demski@outlook.com>
2020-03-19 20:17:24 +0100

    Rebuilt project to use spring security

--- src/main/java/pl/okpol/mdplanner/MdPlannerApplication.java
+++ src/main/java/pl/okpol/mdplanner/MdPlannerApplication.java
@@ -1,17 +1,9 @@
 package pl.okpol.mdplanner;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
 
-@Configuration
-@EnableWebMvc
+
 @SpringBootApplication
 public class MdPlannerApplication {
 
@@ -19,12 +11,4 @@ public class MdPlannerApplication {
         SpringApplication.run(MdPlannerApplication.class, args);
     }
 
-    @Bean
-    public InternalResourceViewResolver viewResolver() {
-        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setViewClass(JstlView.class);
-        viewResolver.setPrefix("/WEB-INF/views/");
-        viewResolver.setSuffix(".jsp");
-        return viewResolver;
-    }
 }
