042c24fb8b6a129cdae436432cb685687ed2c092
mardem <m.demski@outlook.com>
2020-03-19 18:08:54 +0100

    Deleted one class with configuration and added in main one

--- src/main/java/pl/okpol/mdplanner/MdPlannerApplication.java
+++ src/main/java/pl/okpol/mdplanner/MdPlannerApplication.java
@@ -1,8 +1,17 @@
 package pl.okpol.mdplanner;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
+@Configuration
+@EnableWebMvc
 @SpringBootApplication
 public class MdPlannerApplication {
 
@@ -10,4 +19,12 @@ public class MdPlannerApplication {
         SpringApplication.run(MdPlannerApplication.class, args);
     }
 
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
 }
