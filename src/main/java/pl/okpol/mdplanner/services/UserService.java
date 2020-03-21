44481ffa18e60c29812d2ccffb60d08dd5c018eb
mardem <m.demski@outlook.com>
2020-03-21 14:19:48 +0100

    UserService class added

---
+++ src/main/java/pl/okpol/mdplanner/services/UserService.java
@@ -0,0 +1,28 @@
+package pl.okpol.mdplanner.services;
+
+import org.springframework.stereotype.Service;
+import pl.okpol.mdplanner.model.User;
+import pl.okpol.mdplanner.repositories.UserRepository;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class UserService {
+
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User getUserByEmail(String email) {
+        return userRepository.getByEmail(email);
+    }
+
+    public Optional findById(Long id) {
+        return userRepository.findById(id);
+    }
+
+}
