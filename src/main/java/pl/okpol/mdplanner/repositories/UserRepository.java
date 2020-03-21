854dfaa73e4e1a24ccc9ba58ac56322fbb081759
mardem <m.demski@outlook.com>
2020-03-21 14:19:22 +0100

    New method added

--- src/main/java/pl/okpol/mdplanner/repositories/UserRepository.java
+++ src/main/java/pl/okpol/mdplanner/repositories/UserRepository.java
@@ -4,7 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import pl.okpol.mdplanner.model.AbstractEntity;
 import pl.okpol.mdplanner.model.User;
 
+import java.util.Optional;
+
 public interface UserRepository<T extends AbstractEntity, L extends Number> extends JpaRepository<User, Long> {
 
     long countByEmail(String email);
+    User getByEmail(String email);
+    Optional<User> findById(Long userId);
 }
