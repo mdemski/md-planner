30d079838c6a789e4ac238c88e82045e23c740ee
mardem <m.demski@outlook.com>
2020-03-21 11:49:35 +0100

    AbstractEntity added

--- src/main/java/pl/okpol/mdplanner/repositories/UserRepository.java
+++ src/main/java/pl/okpol/mdplanner/repositories/UserRepository.java
@@ -1,9 +1,10 @@
 package pl.okpol.mdplanner.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import pl.okpol.mdplanner.model.AbstractEntity;
 import pl.okpol.mdplanner.model.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository<T extends AbstractEntity, L extends Number> extends JpaRepository<User, Long> {
 
     long countByEmail(String email);
 }
