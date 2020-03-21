e5ad8badb854127ab83d1fcbb3261729c9c26988
mardem <m.demski@outlook.com>
2020-03-21 12:08:15 +0100

    Localization changed

--- src/main/java/pl/okpol/mdplanner/MdPlannerApplication.java
+++ src/main/java/pl/okpol/mdplanner/MdPlannerApplication.java
@@ -1,4 +1,4 @@
-package pl.okpol.mdplanner.config;
+package pl.okpol.mdplanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
