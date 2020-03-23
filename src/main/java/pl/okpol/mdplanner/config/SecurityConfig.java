94c0758f450aa2f97db7cd016881445e3eff8a8e
mardem <m.demski@outlook.com>
2020-03-23 22:16:34 +0100

    Logout success url added

--- src/main/java/pl/okpol/mdplanner/config/SecurityConfig.java
+++ src/main/java/pl/okpol/mdplanner/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package pl.okpol.mdplanner.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,20 +10,15 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.DefaultRedirectStrategy;
-import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import pl.okpol.mdplanner.converters.MySimpleUrlAuthenticationSuccessHandler;
 import pl.okpol.mdplanner.repositories.IAuthenticationFacade;
 import pl.okpol.mdplanner.repositories.UserRepository;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
-import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -81,6 +77,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .successHandler(myAuthenticationSuccessHandler())
                 .failureUrl("/logowanie")
                 .and().logout()
+                .logoutSuccessUrl("/")
                 .and().csrf().disable();
     }
 
