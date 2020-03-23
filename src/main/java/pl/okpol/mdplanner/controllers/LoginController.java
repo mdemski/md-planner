7cf6afed07e708a005f522165f8b59f07abf1560
mardem <m.demski@outlook.com>
2020-03-23 22:17:28 +0100

    Checking email in database added

--- src/main/java/pl/okpol/mdplanner/controllers/LoginController.java
+++ src/main/java/pl/okpol/mdplanner/controllers/LoginController.java
@@ -4,14 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.okpol.mdplanner.dto.UserLoginDTO;
-import pl.okpol.mdplanner.model.User;
 import pl.okpol.mdplanner.services.UserService;
 
 import javax.validation.Valid;
-import java.security.Principal;
 
 @Controller
 @RequestMapping("/logowanie")
@@ -20,17 +19,21 @@ public class LoginController {
     private UserService userService;
 
     @GetMapping
-    public String prepareLoginPage(Model model, Principal principal) {
+    public String prepareLoginPage(Model model) {
         model.addAttribute("loginData", new UserLoginDTO());
         return "login";
     }
 
     @PostMapping
-    public String processLoginPage(@Valid UserLoginDTO userLoginDTO, BindingResult result) {
+    public String processLoginPage(@ModelAttribute("loginData") @Valid UserLoginDTO loginData, BindingResult result) {
         if (!result.hasErrors()) {
-            User userName = userService.getUserByEmail(userLoginDTO.getEmail());
             return "redirect:/zamowienia";
         } else {
+            if (loginData.getEmail() == null) {
+                result.rejectValue("email", null, "Brak adresu w bazie danych. Zarejestruj konto.");
+            }
+            result.rejectValue("email", null, "Podany email lub hasło są niepoprawne.");
+            result.rejectValue("password", null, "Podany email lub hasło są niepoprawne.");
             return "login";
         }
     }
