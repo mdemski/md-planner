2d5058d28badfd20dd7ee89de03861e572969a2b
mardem <m.demski@outlook.com>
2020-03-30 20:41:42 +0200

    Refactor orderDTO class

--- src/main/java/pl/okpol/mdplanner/dto/OrderDTO.java
+++ src/main/java/pl/okpol/mdplanner/dto/OrderDTO.java
@@ -1,60 +1,85 @@
 package pl.okpol.mdplanner.dto;
 
-import com.opencsv.bean.CsvBindByName;
+import pl.okpol.mdplanner.model.Pallet;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public class OrderDTO {
 
-    @CsvBindByName
-    private Integer offer;
-    @CsvBindByName
-    private String reference;
-    @CsvBindByName
+    private Long id;
+    private Integer offerNumber;
+    private String referenceNumber;
     private Integer number;
-    @CsvBindByName
     private String client;
-    @CsvBindByName
-    private String system;
-    @CsvBindByName
+    private String profileSystem;
     private String colour;
-    @CsvBindByName
-    private Double units;
-    @CsvBindByName
-    private Integer windows;
-    @CsvBindByName
-    private Integer doors;
-    @CsvBindByName
-    private Integer slidings;
+    private LocalDate profileDatedDelivery;
+    private LocalDate hardwareDatedDelivery;
+    private LocalDate glazingDatedDelivery;
+    private LocalDate extrasDatedDelivery;
+    private Integer optimizationNumber;
+    private Double windowUnits;
+    private Integer numberOfWindows;
+    private Integer numberOfDoors;
+    private Integer numberOfSlidingDoors;
+    private LocalDate productionTime;
+    private LocalDate dateOfShipment;
+    private Integer expectationWeekNumber;
+    private List<Pallet> pallets;
+    private boolean completed;
+    private String comments;
 
-    public OrderDTO(Integer offer, String reference, Integer number, String client, String system, String colour, Double units, Integer windows, Integer numberOfDoors, Integer slidings) {
-        this.offer = offer;
-        this.reference = reference;
+    public OrderDTO(Long id, Integer offerNumber, String referenceNumber, Integer number, String client, String profileSystem, String colour, LocalDate profileDatedDelivery, LocalDate hardwareDatedDelivery, LocalDate glazingDatedDelivery, LocalDate extrasDatedDelivery, Integer optimizationNumber, Double windowUnits, Integer numberOfWindows, Integer numberOfDoors, Integer numberOfSlidingDoors, LocalDate productionTime, LocalDate dateOfShipment, Integer expectationWeekNumber, List<Pallet> pallets, boolean completed, String comments) {
+        this.id = id;
+        this.offerNumber = offerNumber;
+        this.referenceNumber = referenceNumber;
         this.number = number;
         this.client = client;
-        this.system = system;
+        this.profileSystem = profileSystem;
         this.colour = colour;
-        this.units = units;
-        this.windows = windows;
-        this.doors = numberOfDoors;
-        this.slidings = slidings;
+        this.profileDatedDelivery = profileDatedDelivery;
+        this.hardwareDatedDelivery = hardwareDatedDelivery;
+        this.glazingDatedDelivery = glazingDatedDelivery;
+        this.extrasDatedDelivery = extrasDatedDelivery;
+        this.optimizationNumber = optimizationNumber;
+        this.windowUnits = windowUnits;
+        this.numberOfWindows = numberOfWindows;
+        this.numberOfDoors = numberOfDoors;
+        this.numberOfSlidingDoors = numberOfSlidingDoors;
+        this.productionTime = productionTime;
+        this.dateOfShipment = dateOfShipment;
+        this.expectationWeekNumber = expectationWeekNumber;
+        this.pallets = pallets;
+        this.completed = completed;
+        this.comments = comments;
     }
 
     public OrderDTO() {
     }
 
-    public Integer getOffer() {
-        return offer;
+    public Long getId() {
+        return id;
     }
 
-    public void setOffer(Integer offer) {
-        this.offer = offer;
+    public void setId(Long id) {
+        this.id = id;
     }
 
-    public String getReference() {
-        return reference;
+    public Integer getOfferNumber() {
+        return offerNumber;
     }
 
-    public void setReference(String reference) {
-        this.reference = reference;
+    public void setOfferNumber(Integer offerNumber) {
+        this.offerNumber = offerNumber;
+    }
+
+    public String getReferenceNumber() {
+        return referenceNumber;
+    }
+
+    public void setReferenceNumber(String referenceNumber) {
+        this.referenceNumber = referenceNumber;
     }
 
     public Integer getNumber() {
@@ -73,12 +98,12 @@ public class OrderDTO {
         this.client = client;
     }
 
-    public String getSystem() {
-        return system;
+    public String getProfileSystem() {
+        return profileSystem;
     }
 
-    public void setSystem(String system) {
-        this.system = system;
+    public void setProfileSystem(String profileSystem) {
+        this.profileSystem = profileSystem;
     }
 
     public String getColour() {
@@ -89,35 +114,123 @@ public class OrderDTO {
         this.colour = colour;
     }
 
-    public Double getUnits() {
-        return units;
+    public LocalDate getProfileDatedDelivery() {
+        return profileDatedDelivery;
     }
 
-    public void setUnits(Double units) {
-        this.units = units;
+    public void setProfileDatedDelivery(LocalDate profileDatedDelivery) {
+        this.profileDatedDelivery = profileDatedDelivery;
     }
 
-    public Integer getWindows() {
-        return windows;
+    public LocalDate getHardwareDatedDelivery() {
+        return hardwareDatedDelivery;
     }
 
-    public void setWindows(Integer windows) {
-        this.windows = windows;
+    public void setHardwareDatedDelivery(LocalDate hardwareDatedDelivery) {
+        this.hardwareDatedDelivery = hardwareDatedDelivery;
     }
 
-    public Integer getDoors() {
-        return doors;
+    public LocalDate getGlazingDatedDelivery() {
+        return glazingDatedDelivery;
     }
 
-    public void setDoors(Integer numberOfDoors) {
-        this.doors = numberOfDoors;
+    public void setGlazingDatedDelivery(LocalDate glazingDatedDelivery) {
+        this.glazingDatedDelivery = glazingDatedDelivery;
     }
 
-    public Integer getSlidings() {
-        return slidings;
+    public LocalDate getExtrasDatedDelivery() {
+        return extrasDatedDelivery;
     }
 
-    public void setSlidings(Integer slidings) {
-        this.slidings = slidings;
+    public void setExtrasDatedDelivery(LocalDate extrasDatedDelivery) {
+        this.extrasDatedDelivery = extrasDatedDelivery;
+    }
+
+    public Integer getOptimizationNumber() {
+        return optimizationNumber;
+    }
+
+    public void setOptimizationNumber(Integer optimizationNumber) {
+        this.optimizationNumber = optimizationNumber;
+    }
+
+    public Double getWindowUnits() {
+        return windowUnits;
+    }
+
+    public void setWindowUnits(Double windowUnits) {
+        this.windowUnits = windowUnits;
+    }
+
+    public Integer getNumberOfWindows() {
+        return numberOfWindows;
+    }
+
+    public void setNumberOfWindows(Integer numberOfWindows) {
+        this.numberOfWindows = numberOfWindows;
+    }
+
+    public Integer getNumberOfDoors() {
+        return numberOfDoors;
+    }
+
+    public void setNumberOfDoors(Integer numberOfDoors) {
+        this.numberOfDoors = numberOfDoors;
+    }
+
+    public Integer getNumberOfSlidingDoors() {
+        return numberOfSlidingDoors;
+    }
+
+    public void setNumberOfSlidingDoors(Integer numberOfSlidingDoors) {
+        this.numberOfSlidingDoors = numberOfSlidingDoors;
+    }
+
+    public LocalDate getProductionTime() {
+        return productionTime;
+    }
+
+    public void setProductionTime(LocalDate productionTime) {
+        this.productionTime = productionTime;
+    }
+
+    public LocalDate getDateOfShipment() {
+        return dateOfShipment;
+    }
+
+    public void setDateOfShipment(LocalDate dateOfShipment) {
+        this.dateOfShipment = dateOfShipment;
+    }
+
+    public Integer getExpectationWeekNumber() {
+        return expectationWeekNumber;
+    }
+
+    public void setExpectationWeekNumber(Integer expectationWeekNumber) {
+        this.expectationWeekNumber = expectationWeekNumber;
+    }
+
+    public List<Pallet> getPallets() {
+        return pallets;
+    }
+
+    public void setPallets(List<Pallet> pallets) {
+        this.pallets = pallets;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
     }
 }
