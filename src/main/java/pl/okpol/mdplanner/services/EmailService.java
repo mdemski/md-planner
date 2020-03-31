7693ad629e3e81e3c144a949df8d469c8177abda
mardem <m.demski@outlook.com>
2020-03-31 21:40:23 +0200

    EmailService added

---
+++ src/main/java/pl/okpol/mdplanner/services/EmailService.java
@@ -0,0 +1,50 @@
+package pl.okpol.mdplanner.services;
+
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import pl.okpol.mdplanner.model.User;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Service
+public class EmailService {
+
+    private JavaMailSender javaMailSender;
+    private UserService userService;
+
+    public EmailService(JavaMailSender javaMailSender, UserService userService) {
+        this.javaMailSender = javaMailSender;
+        this.userService = userService;
+    }
+
+    public void sendActiveUser(String email, String serverAddress) {
+        User user = userService.findByEmail(email);
+        String htmlMsg = "Naciśnij <a href=\""
+                + serverAddress + "/users/register_process?userEmail="
+                + email + "&userUUID="
+                + user.getUuid().toString()
+                + "\">link</a> żeby aktywować konto";
+        String title = "MDPlanner - link do aktywacji konta";
+        sendHTMLMessage(email,htmlMsg,title);
+    }
+
+
+    public void sendHTMLMessage(String email, String message, String title) {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
+        String htmlMsg = message;
+        try {
+            mimeMessage.setContent(htmlMsg, "text/html;charset=utf-8");
+            helper.setTo(email);
+            helper.setSubject(title);
+            helper.setFrom("m.demski@outlook.com");
+            javaMailSender.send(mimeMessage);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
