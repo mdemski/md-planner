5ec83030886a8b0fb306823d6c3fe3cf4f652707
mardem <m.demski@outlook.com>
2020-04-01 19:54:58 +0200

    Setting activated after process

--- src/main/java/pl/okpol/mdplanner/controllers/RegistrationProcessController.java
+++ src/main/java/pl/okpol/mdplanner/controllers/RegistrationProcessController.java
@@ -6,15 +6,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import pl.okpol.mdplanner.services.RegistrationService;
+import pl.okpol.mdplanner.services.UserService;
 
 @Controller
 @RequestMapping("/proces-rejestracji")
 public class RegistrationProcessController {
 
     private RegistrationService registrationService;
+    private UserService userService;
 
-    public RegistrationProcessController(RegistrationService registrationService) {
+    public RegistrationProcessController(RegistrationService registrationService, UserService userService) {
         this.registrationService = registrationService;
+        this.userService = userService;
     }
 
     @GetMapping
@@ -24,6 +27,7 @@ public class RegistrationProcessController {
         if (userEmail != null) {
             if (registrationService.checkEmailUUID(userEmail, UUID)) {
                 model.addAttribute("successMessage", "Konto zarejestrowane poprawnie");
+                registrationService.setToActivated(userService.getUserByEmail(userEmail));
             }
             else {
                 model.addAttribute("errorMessage", "Nie udało się poprawnie założyć konta, spróbuj ponownie");
