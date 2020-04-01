5026fb1302ce8471acc68952a3f5c5fbf44f1193
mardem <m.demski@outlook.com>
2020-04-01 23:04:05 +0200

    Set configuration to mail server.

--- src/main/java/pl/okpol/mdplanner/config/MailConfig.java
+++ src/main/java/pl/okpol/mdplanner/config/MailConfig.java
@@ -2,20 +2,32 @@ package pl.okpol.mdplanner.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import java.util.Properties;
 
 @Configuration
+@PropertySource("classpath:email.properties")
 public class MailConfig {
+
+    private Environment environment;
+
+    public MailConfig(Environment environment) {
+        this.environment = environment;
+    }
 
     @Bean
     public JavaMailSender javaMailService() {
         JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
 
-        javaMailSender.setHost("myHost");
-        javaMailSender.setPort(25);
+        javaMailSender.setHost("mail.okpol.com");
+        javaMailSender.setPort(587);
+
+        javaMailSender.setUsername(environment.getProperty("spring.mail.username"));
+        javaMailSender.setPassword(environment.getProperty("spring.mail.password"));
 
         javaMailSender.setJavaMailProperties(getMailProperties());
 
