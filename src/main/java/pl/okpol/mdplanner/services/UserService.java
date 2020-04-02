11027b30490f1dab37e93375a0ce55e059725635
mardem <m.demski@outlook.com>
2020-04-02 19:58:18 +0200

    Rebuilt user service - registration service deleted

--- src/main/java/pl/okpol/mdplanner/services/UserService.java
+++ src/main/java/pl/okpol/mdplanner/services/UserService.java
@@ -1,10 +1,14 @@
 package pl.okpol.mdplanner.services;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import pl.okpol.mdplanner.dto.UserDTO;
 import pl.okpol.mdplanner.model.User;
 import pl.okpol.mdplanner.repositories.UserRepository;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -12,9 +16,11 @@ import java.util.Optional;
 public class UserService {
 
     private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public User getUserByEmail(String email) {
@@ -27,5 +33,39 @@ public class UserService {
 
     public User findByEmail(String email) {
         return userRepository.findByEmail(email);
+    }
+
+    public boolean isEmailAvailable(String email) {
+        long countEmail = userRepository.countByEmail(email);
+        if (countEmail > 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public void registerUser(UserDTO data) {
+        User user = new User();
+        user.setEmail(data.getEmail());
+        String encodedPassword = passwordEncoder.encode(data.getPassword());
+        user.setPassword(encodedPassword);
+        user.setFirstName(data.getFirstName());
+        user.setRole(data.getRole());
+        user.setActivated(data.isActivated());
+        List<User> users = new ArrayList<>();
+        users.add(user);
+        userRepository.save(user);
+    }
+
+    public boolean checkEmailUUID(String userEmail, String uuid) {
+        return userRepository.getByEmail(userEmail).getUuid().equals(uuid);
+    }
+
+    public void setToActivated(User userByEmail) {
+        userByEmail.setActivated(true);
+    }
+
+    public void updateUser(User user) {
+        userRepository.save(user);
     }
 }
