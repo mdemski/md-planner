6b31e7d209c8e7e9dde040e87ac605fa2d262f4a
mdemski <m.demski@outlook.com>
2020-04-22 22:02:36 +0200

    Native query added

--- src/main/java/pl/okpol/mdplanner/repositories/OrderRepository.java
+++ src/main/java/pl/okpol/mdplanner/repositories/OrderRepository.java
@@ -1,8 +1,14 @@
 package pl.okpol.mdplanner.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pl.okpol.mdplanner.model.AbstractEntity;
 import pl.okpol.mdplanner.model.Order;
 
 public interface OrderRepository<T extends AbstractEntity, L extends Number> extends JpaRepository<Order, Long> {
+    
+    @Query(value = "SELECT * FROM orders WHERE completed = 0", nativeQuery = true)
+    Page<Order> findAllByInCompleted(Pageable pageable);
 }
