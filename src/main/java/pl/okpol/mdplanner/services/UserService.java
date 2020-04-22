7c8cb9f40f1c685774c4b5718f0a5676d03fb615
mdemski <m.demski@outlook.com>
2020-04-22 21:59:14 +0200

    Find one user method refactored. Changed uuid file type in hole application. Added find all users method

--- src/main/java/pl/okpol/mdplanner/services/UserService.java
+++ src/main/java/pl/okpol/mdplanner/services/UserService.java
@@ -1,5 +1,6 @@
 package pl.okpol.mdplanner.services;
 
+import ch.qos.logback.core.util.COWArrayList;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import pl.okpol.mdplanner.dto.UserDTO;
@@ -9,12 +10,12 @@ import pl.okpol.mdplanner.repositories.UserRepository;
 import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @Transactional
 public class UserService {
 
+    public COWArrayList<Object> findAllUsers;
     private UserRepository userRepository;
     private PasswordEncoder passwordEncoder;
 
@@ -27,21 +28,8 @@ public class UserService {
         return userRepository.getByEmail(email);
     }
 
-    public Optional findById(Long id) {
-        return userRepository.findById(id);
-    }
-
     public User findByEmail(String email) {
         return userRepository.findByEmail(email);
-    }
-
-    public boolean isEmailAvailable(String email) {
-        long countEmail = userRepository.countByEmail(email);
-        if (countEmail > 0) {
-            return false;
-        } else {
-            return true;
-        }
     }
 
     public void registerUser(UserDTO data) {
@@ -52,20 +40,17 @@ public class UserService {
         user.setFirstName(data.getFirstName());
         user.setRole(data.getRole());
         user.setActivated(data.isActivated());
+        user.setUuid(data.getUuid());
         List<User> users = new ArrayList<>();
         users.add(user);
         userRepository.save(user);
     }
 
-    public boolean checkEmailUUID(String userEmail, String uuid) {
-        return userRepository.getByEmail(userEmail).getUuid().equals(uuid);
-    }
-
-    public void setToActivated(User userByEmail) {
-        userByEmail.setActivated(true);
-    }
-
     public void updateUser(User user) {
         userRepository.save(user);
+    }
+
+    public List<User> findAllUsers() {
+        return userRepository.findAll();
     }
 }
