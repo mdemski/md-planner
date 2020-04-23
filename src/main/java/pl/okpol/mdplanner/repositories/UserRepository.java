2611117e69ea7b04922408bd230ef976873de7b0
mdemski <m.demski@outlook.com>
2020-04-23 21:54:36 +0200

    One method deleted

--- src/main/java/pl/okpol/mdplanner/repositories/UserRepository.java
+++ src/main/java/pl/okpol/mdplanner/repositories/UserRepository.java
@@ -1,15 +1,14 @@
 package pl.okpol.mdplanner.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import pl.okpol.mdplanner.model.AbstractEntity;
 import pl.okpol.mdplanner.model.User;
 
 import java.util.Optional;
 
-public interface UserRepository<T extends AbstractEntity, L extends Number> extends JpaRepository<User, Long> {
+
+public interface UserRepository extends JpaRepository<User, Long> {
 
     long countByEmail(String email);
     User getByEmail(String email);
-    Optional<User> findById(Long userId);
     User findByEmail(String email);
 }
