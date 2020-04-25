a6bd2e25e62eb5a3e735e8d333abb843ac550f6f
mdemski <m.demski@outlook.com>
2020-04-25 14:36:02 +0200

    Refactoring

--- src/main/java/pl/okpol/mdplanner/controllers/RegistrationController.java
+++ src/main/java/pl/okpol/mdplanner/controllers/RegistrationController.java
@@ -1,83 +1,77 @@
 package pl.okpol.mdplanner.controllers;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 import pl.okpol.mdplanner.dto.UserDTO;
+import pl.okpol.mdplanner.mappers.UserMapper;
 import pl.okpol.mdplanner.model.User;
 import pl.okpol.mdplanner.services.EmailService;
 import pl.okpol.mdplanner.services.UserService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
-@Controller
-@RequestMapping("/rejestracja")
+@RestController
+@RequestMapping("/api/users")
+@CrossOrigin
 public class RegistrationController {
 
     private UserService userService;
     private EmailService emailService;
+    private UserMapper userMapper;
 
-    public RegistrationController(UserService userService, EmailService emailService) {
+    public RegistrationController(UserService userService, EmailService emailService, UserMapper userMapper) {
         this.userService = userService;
         this.emailService = emailService;
+        this.userMapper = userMapper;
     }
 
-    @GetMapping
-    public String prepareRegistrationForm(Model model) {
+    @GetMapping("/list-of-roles")
+    public List<String> prepareRegistrationForm() {
         List<String> roles = new ArrayList<>();
         roles.add("zaopatrzenie");
         roles.add("handel");
         roles.add("produkcja");
         roles.add("jakość");
-        model.addAttribute("data", new UserDTO(false));
-        model.addAttribute("roles", roles);
-        return "registration";
+        return roles;
     }
 
-    @PostMapping()
-    public String processRegistrationPage(@ModelAttribute("data") @Valid UserDTO data, BindingResult result, HttpServletRequest servletRequest) {
-        if (result.hasErrors()) { //walidacja poprawności wpisanych danych
-            return "registration";
-        } else if (data.getFirstName().equals("")) {
-            result.rejectValue("firstName", null, "Podaj imię");
-            return "registration";
-        } else if (!data.getPassword().equals(data.getRePassword())) { //walidacja poprawności haseł
-            result.rejectValue("rePassword", null, "Hasło i powtórzone hasło muszą być takie same");
-            return "registration";
-        } else if (!userService.isEmailAvailable(data.getEmail())) { //walidacja zajętości email
-            result.rejectValue("email", null, "Email jest już zajęty");
-            return "registration";
-        } else {
-            String serverAddress = servletRequest.getRequestURL().substring(0, servletRequest.getRequestURL().length() - servletRequest.getRequestURI().length()) + servletRequest.getServletContext().getContextPath();
-            userService.registerUser(data);
-            emailService.sendActiveUser(data.getEmail(), serverAddress);
-            return "redirect:/rejestracja/proces-rejestracji";
-        }
+    @GetMapping
+    public List<User> prepareAllUsersList() {
+        return userService.findAllUsers();
     }
 
-    @GetMapping("/proces-rejestracji")
-    public String registrationProcessPage() {
-        return "registerProcess";
-    }
-
-    @GetMapping("/aktywacja?userEmail={userEmail}&userUUID={UUID}")
-    public String registrationProcessPage(@PathVariable(required = false, name = "UUID") String UUID,
-                                          @PathVariable(required = false, name = "userEmail") String userEmail,
-                                          Model model) {
-        if (userEmail != null) {
-            if (userService.checkEmailUUID(userEmail, UUID)) {
-                model.addAttribute("successMessage", "Konto zarejestrowane poprawnie");
-                User user = userService.getUserByEmail(userEmail);
-                userService.setToActivated(user);
-                userService.updateUser(user);
-            } else {
-                model.addAttribute("errorMessage", "Nie udało się poprawnie założyć konta, spróbuj ponownie");
+    @PostMapping
+    public User registerUser(@Valid @RequestBody UserDTO userDTO,
+                             BindingResult result,
+                             HttpServletRequest servletRequest) {
+        if (result.hasErrors()) {
+            for (ObjectError error : result.getAllErrors()) {
+                System.out.println(error.toString());
             }
         }
-        return "/login";
+        String serverAddress = servletRequest.getRequestURL()
+                .substring(0, servletRequest.getRequestURL().length() - servletRequest.getRequestURI().length())
+                + servletRequest.getServletContext().getContextPath();
+        emailService.sendActiveUser(userDTO.getEmail(), userDTO.getUuid(), serverAddress);
+        userService.registerUser(userDTO);
+        return userMapper.convertToUserEntity(userDTO);
+    }
+
+    @GetMapping("/activate/{activationKey}")
+    public String processActivationUser(@PathVariable String activationKey) {
+        String[] emailAndUuid = activationKey.split("&");
+        String email = emailAndUuid[0];
+        String uuid = emailAndUuid[1];
+        User user = userService.getUserByEmail(email);
+        if (user != null) {
+            if (user.getUuid().equals(uuid)) {
+                user.setActivated(true);
+                userService.updateUser(user);
+            }
+        }
+        return "Udało się aktywować konto. Zaloguj się";
     }
 }
