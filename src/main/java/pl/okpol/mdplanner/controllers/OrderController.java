8cb1435e839f758f53f8b97a50d15ba2d85ab4fc
mdemski <m.demski@outlook.com>
2020-04-26 16:09:58 +0200

    Get mapping by number order added

--- src/main/java/pl/okpol/mdplanner/controllers/OrderController.java
+++ src/main/java/pl/okpol/mdplanner/controllers/OrderController.java
@@ -4,60 +4,76 @@ import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import pl.okpol.mdplanner.dto.AddedOrderDTO;
+import pl.okpol.mdplanner.dto.OrderDTO;
+import pl.okpol.mdplanner.mappers.OrderMapper;
+import pl.okpol.mdplanner.model.Order;
 import pl.okpol.mdplanner.services.OrderService;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.util.ArrayList;
 import java.util.List;
 
-@Controller
-@RequestMapping("/zamowienia")
+@RestController
+@RequestMapping("/api/orders")
+@CrossOrigin
 public class OrderController {
 
     private OrderService orderService;
+    private OrderMapper orderMapper;
 
-    public OrderController(OrderService orderService) {
+    public OrderController(OrderService orderService, OrderMapper orderMapper) {
         this.orderService = orderService;
+        this.orderMapper = orderMapper;
     }
 
     @GetMapping
-    public String prepareOrdersPage(Model model) {
-        model.addAttribute("orders", orderService.getAllToProduceOrders());
-        return "orders";
+    public List<Order> prepareOrdersPage() {
+        List<OrderDTO> orderDTOS = orderService.getAllOrders();
+        return orderMapper.convertToEntityOrders(orderDTOS);
     }
 
     @PostMapping
-    public String processUploadFile(@RequestParam("file") MultipartFile file, Model model) {
+    public List<Order> processUploadFile(@RequestParam("file") MultipartFile file, Model model) {
 
         if(file.isEmpty()){
-            model.addAttribute("message", "Wybierz plik CSV do wczytania");
-            model.addAttribute("status", false);
+            System.out.println("Wybierz plik CSV do wczytania");
         } else {
             // parse CSV file to create a list of `User` objects
             try (Reader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
-
                 CsvToBean<AddedOrderDTO> csvToBean = new CsvToBeanBuilder(reader)
                         .withType(AddedOrderDTO.class)
                         .withIgnoreLeadingWhiteSpace(true)
                         .build();
                 List<AddedOrderDTO> newOrders = csvToBean.parse();
 
-                for (AddedOrderDTO newOrder : newOrders) {
+                newOrders.forEach((newOrder) -> {
                     orderService.saveOrderInDB(newOrder);
-                }
+                });
+//                for (AddedOrderDTO newOrder : newOrders) {
+//                    orderService.saveOrderInDB(newOrder);
+//                }
+                return orderMapper.convertToListEntityOrderFromAddDTO(newOrders);
 
             } catch (Exception e) {
-                model.addAttribute("message", "Nie udało się wczytać pliku");
-                model.addAttribute("status", false);
+                System.out.println("Failed with uploading file");
             }
         }
-        return "redirect:/zamowienia";
+        return new ArrayList<>();
+    }
+
+    @GetMapping("/{number}")
+    public Order processOrderDetail(@PathVariable Integer number) {
+        try {
+            Order order = orderService.findOneByNumber(number);
+            return order;
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+        return null;
     }
 }
