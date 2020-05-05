95c8edf93df2a7402bc5719f7464795d7631256d
mdemski <m.demski@outlook.com>
2020-05-05 18:03:57 +0200

    Changed date format for hole application

--- src/main/java/pl/okpol/mdplanner/MdPlannerApplication.java
+++ src/main/java/pl/okpol/mdplanner/MdPlannerApplication.java
@@ -1,10 +1,20 @@
 package pl.okpol.mdplanner;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import pl.okpol.mdplanner.mappers.LocalDateDeserializer;
+import pl.okpol.mdplanner.mappers.LocalDateSerializer;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static java.time.format.DateTimeFormatter.ofPattern;
 
 
 @SpringBootApplication
@@ -17,5 +27,18 @@ public class MdPlannerApplication {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    public static final DateTimeFormatter FORMATTER = ofPattern("MM/dd/yyyy");
+
+    @Bean
+    @Primary
+    public ObjectMapper serializingObjectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        javaTimeModule.addSerializer(LocalDate.class, new LocalDateSerializer());
+        javaTimeModule.addDeserializer(LocalDate.class, new LocalDateDeserializer());
+        objectMapper.registerModule(javaTimeModule);
+        return objectMapper;
     }
 }
