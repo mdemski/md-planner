d82cf7ae013abe8020fa93196cb7ccb00359b1d4
mdemski <m.demski@outlook.com>
2020-05-07 22:37:28 +0200

    Set default time zone

--- src/main/java/pl/okpol/mdplanner/mappers/DateConverter.java
+++ src/main/java/pl/okpol/mdplanner/mappers/DateConverter.java
@@ -1,13 +1,17 @@
 package pl.okpol.mdplanner.mappers;
 
 import java.time.LocalDate;
+import java.util.Locale;
 
 public class DateConverter {
+
     public static LocalDate convertFromStringToDate(String date) {
+        Locale.setDefault(Locale.GERMANY);
         return LocalDate.parse(date);
     }
 
     public static String convertFromDateToString(LocalDate date) {
+        Locale.setDefault(Locale.GERMANY);
         return date.toString();
     }
 
