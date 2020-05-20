bbc5fc4ea88d84df57612cd7e01c469ecd095d4d
mdemski <m.demski@outlook.com>
2020-05-20 17:04:14 +0200

    Initialize method deleted

--- src/main/java/pl/okpol/mdplanner/services/OrderService.java
+++ src/main/java/pl/okpol/mdplanner/services/OrderService.java
@@ -6,13 +6,11 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import pl.okpol.mdplanner.dto.AddedOrderDTO;
 import pl.okpol.mdplanner.dto.OrderDTO;
-import pl.okpol.mdplanner.mappers.PalletMapper;
 import pl.okpol.mdplanner.model.Order;
 import pl.okpol.mdplanner.repositories.OrderRepository;
 
 import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,12 +19,10 @@ import java.util.stream.Collectors;
 public class OrderService {
 
     private OrderRepository orderRepository;
-    private PalletMapper palletMapper;
 //    private static final LocalDate defaultDate = LocalDate.parse("01-01-1991");
 
-    public OrderService(OrderRepository orderRepository, PalletMapper palletMapper) {
+    public OrderService(OrderRepository orderRepository) {
         this.orderRepository = orderRepository;
-        this.palletMapper = palletMapper;
     }
 
     public void saveOrderInDB(AddedOrderDTO addedOrderDTO) {
@@ -134,7 +130,7 @@ public class OrderService {
             dto.setProductionTime(source.getProductionTime());
             dto.setDateOfShipment(source.getDateOfShipment());
             dto.setExpectationWeekNumber(source.getExpectationWeekNumber());
-            dto.setPallets(palletMapper.fromPalletsToStringConverter(source.getPallets()));
+            dto.setPallets(source.getPallets());
             dto.setCompleted(source.isCompleted());
             dto.setComments(source.getComments());
             return dto;
@@ -158,8 +154,8 @@ public class OrderService {
         order.setOptimizationNumber(orderDTO.getOptimizationNumber());
         order.setProductionTime(orderDTO.getProductionTime());
         order.setDateOfShipment(orderDTO.getDateOfShipment());
-        if (order.getPallets() != palletMapper.fromStringToPalletsConverter(orderDTO.getPallets())) {
-            order.setPallets(palletMapper.fromStringToPalletsConverter(orderDTO.getPallets()));
+        if (order.getPallets() != orderDTO.getPallets()) {
+            order.setPallets(orderDTO.getPallets());
         }
         order.setCompleted(orderDTO.isCompleted());
         order.setComments(orderDTO.getComments());
@@ -170,69 +166,10 @@ public class OrderService {
         orderRepository.delete(order);
     }
 
-
-    public Order initialize(Order order) {
-        order = orderRepository.findByNumber(order.getNumber());
-        Hibernate.initialize(order.getPallets());
-        return order;
-    }
+//      Potrzebne jeśli wiązanie z inną tabelą FETCH.LAZY - dociąganie danych (do ustalenia czy w serwisie, czy w inne klasie
+//    public Order initialize(Order order) {
+//        order = orderRepository.findByNumber(order.getNumber());
+//        Hibernate.initialize(order.getPallets());
+//        return order;
+//    }
 }
-
-
-//    public List<OrderDTO> getAllOrders() {
-//        Page<Order> ordersDTO = orderRepository.findAllByInCompleted(PageRequest.of(0, 100));
-//        List<Order> content = ordersDTO.getContent();
-//        return content.stream().map(source -> {
-//            OrderDTO dto = new OrderDTO();
-//            dto.setId(source.getId());
-//            dto.setOfferNumber(source.getOfferNumber());
-//            dto.setReferenceNumber(source.getReferenceNumber());
-//            dto.setNumber(source.getNumber());
-//            dto.setClient(source.getClient());
-//            dto.setProfileSystem(source.getProfileSystem());
-//            dto.setColour(source.getColour());
-//            dto.setProfileDatedDelivery(DateConverter.convertFromDateToString(source.getProfileDatedDelivery()));
-//            dto.setHardwareDatedDelivery(DateConverter.convertFromDateToString(source.getHardwareDatedDelivery()));
-//            dto.setGlazingDatedDelivery(DateConverter.convertFromDateToString(source.getGlazingDatedDelivery()));
-//            dto.setExtrasDatedDelivery(DateConverter.convertFromDateToString(source.getExtrasDatedDelivery()));
-//            dto.setOptimizationNumber(source.getOptimizationNumber());
-//            dto.setWindowUnits(source.getWindowUnits());
-//            dto.setNumberOfWindows(source.getNumberOfWindows());
-//            dto.setNumberOfDoors(source.getNumberOfDoors());
-//            dto.setNumberOfSlidingDoors(source.getNumberOfSlidingDoors());
-//            dto.setProductionTime(DateConverter.convertFromDateToString(source.getProductionTime()));
-//            dto.setDateOfShipment(DateConverter.convertFromDateToString(source.getDateOfShipment()));
-//            dto.setExpectationWeekNumber(source.getExpectationWeekNumber());
-//            dto.setPallets(palletMapper.fromPalletsToStringConverter(source.getPallets()));
-//            dto.setCompleted(source.isCompleted());
-//            dto.setComments(source.getComments());
-//            return dto;
-//        }).collect(Collectors.toList());
-//    }
-//
-//    public Order findOneById(Long id) throws Throwable {
-//        return (Order) orderRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("No id"));
-//    }
-//
-//    public Order findOneByNumber(Integer number) {
-//        return orderRepository.findByNumber(number);
-//    }
-//
-//    public void updateOrder(Integer number, OrderDTO orderDTO) {
-//        Order order = orderRepository.findByNumber(number);
-//        order.setProfileDatedDelivery(DateConverter.convertFromStringToDate(orderDTO.getProfileDatedDelivery()));
-//        order.setHardwareDatedDelivery(DateConverter.convertFromStringToDate(orderDTO.getHardwareDatedDelivery()));
-//        order.setGlazingDatedDelivery(DateConverter.convertFromStringToDate(orderDTO.getGlazingDatedDelivery()));
-//        order.setExtrasDatedDelivery(DateConverter.convertFromStringToDate(orderDTO.getExtrasDatedDelivery()));
-//        order.setOptimizationNumber(orderDTO.getOptimizationNumber());
-//        order.setProductionTime(DateConverter.convertFromStringToDate(orderDTO.getProductionTime()));
-//        order.setDateOfShipment(DateConverter.convertFromStringToDate(orderDTO.getDateOfShipment()));
-//        order.setPallets(palletMapper.fromStringToPalletsConverter(orderDTO.getPallets()));
-//        order.setCompleted(orderDTO.isCompleted());
-//        order.setComments(orderDTO.getComments());
-//        orderRepository.save(order);
-//    }
-//
-//    public void deleteOrder(Order order) {
-//        orderRepository.delete(order);
-//    }
